f005f402a081401ad163c43f58b8fa131cdc7703
Daniel Beach <danielbeach@Daniels-MacBook-Pro.local>
2023-03-30 16:46:29 -0500

    clean up code

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,9 @@ use std::{io::{BufRead, BufReader}, fs};
 
 
 pub fn read_number_lines_in_file(file_path: &str) -> u32 {
-    let mut count = 0;
-    let file = std::fs::File::open(file_path).unwrap();
-    let bf = BufReader::new(file);
+    let mut count: u32 = 0;
+    let file: fs::File = std::fs::File::open(file_path).unwrap();
+    let bf: BufReader<fs::File> = BufReader::new(file);
     for _ in bf.lines() {
         count += 1;
     }
@@ -16,18 +16,17 @@ fn remove_quotes(input: &str) -> String {
 }
 
 fn split_line<'a>(line: &'a str, delimiter: &'a str) -> Vec<&'a str> {
-    let line = &line.split(delimiter).collect::<Vec<&str>>();
+    let line: &Vec<&str> = &line.split(delimiter).collect::<Vec<&str>>();
     return line.to_vec();
 }
 
 pub fn print_headers(file_path: &str, delimiter: &str, &quote: &u32) {
-    let file = std::fs::File::open(file_path).unwrap();
-    let bf = BufReader::new(file);
-    let mut count = 0;
+    let file: fs::File = std::fs::File::open(file_path).unwrap();
+    let bf: BufReader<fs::File> = BufReader::new(file);
     for line in bf.lines() {
-        let line = line.unwrap();
+        let line: String = line.unwrap();
         if quote == 1 {
-            let line = remove_quotes(&line);
+            let line: String = remove_quotes(&line);
             println!("Headers: {:?}", split_line(&line, delimiter));
             println!("{}", "");
             break;
@@ -39,18 +38,18 @@ pub fn print_headers(file_path: &str, delimiter: &str, &quote: &u32) {
 
 
 pub fn print_a_few_lines(file_path: &str, delimiter: &str, &quote: &u32, number_of_lines: u32) {
-    let file = std::fs::File::open(file_path).unwrap();
-    let bf = BufReader::new(file);
-    let mut count = 0;
+    let file: fs::File = std::fs::File::open(file_path).unwrap();
+    let bf: BufReader<fs::File> = BufReader::new(file);
+    let mut count: u32 = 0;
     for line in bf.lines() {
         if count == 0 {
             // skip the first line
             count += 1;
             continue;
         }
-        let line = line.unwrap();
+        let line: String = line.unwrap();
         if quote == 1 {
-            let line = remove_quotes(&line);
+            let line: String = remove_quotes(&line);
             println!("Row: {:?}", split_line(&line, delimiter));
             println!("{}", "");
             count += 1;
@@ -69,8 +68,8 @@ pub fn print_a_few_lines(file_path: &str, delimiter: &str, &quote: &u32, number_
 }
 
 pub fn get_file_size_in_mb(file_path: &str) ->f64 {
-    let metadata = fs::metadata(file_path).expect("Error reading file metadata");
-    let file_size = metadata.len() as f64;
-    let mb_size = file_size / (1024.0 * 1024.0);
+    let metadata: fs::Metadata = fs::metadata(file_path).expect("Error reading file metadata");
+    let file_size: f64 = metadata.len() as f64;
+    let mb_size: f64 = file_size / (1024.0 * 1024.0);
     mb_size
 }
